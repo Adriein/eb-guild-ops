66f13ae36c87291f10dbff8a3536386e859e963f
Adriein <adria.claret@gmail.com>
2024-03-28 19:22:50 +0100

    first commit setting the base

--- internal/app_eb_guild_ops/repository/tibia_data_api.go
+++ internal/app_eb_guild_ops/repository/tibia_data_api.go
@@ -22,18 +22,27 @@ type TibiaDataAPI interface {
 	Character() TibiaDataApiResponse[TibiaDataAPICharacter]
 }
 
-const TIBIA_DATA_API_BASE_URL string = "https://api.tibiadata.com/v4"
+const TIBIA_DATA_API_BASE_URL string = "https://api.tibiadata.com"
+const TIBIA_DATA_API_VERSION string = "v4"
 const TIBIA_DATA_API_CHARACTER_URL string = "character"
 const HTTP_GET_METHOD string = "GET"
 
 func Character(name string) TibiaDataApiResponse[TibiaDataAPICharacter] {
 	response, err := http.NewRequest(
 		HTTP_GET_METHOD,
-		fmt.Sprintf("%s/%s", TIBIA_DATA_API_BASE_URL, TIBIA_DATA_API_CHARACTER_URL),
+		fmt.Sprintf(
+			"%s/%s/%s/%s",
+			TIBIA_DATA_API_BASE_URL,
+			TIBIA_DATA_API_VERSION,
+			TIBIA_DATA_API_CHARACTER_URL,
+			name,
+		),
 		nil,
 	)
 
 	if err != nil {
 		return TibiaDataApiResponse[TibiaDataAPICharacter]{nil, false}
 	}
+
+	return TibiaDataApiResponse[TibiaDataAPICharacter]{}
 }
