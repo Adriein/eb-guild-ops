22147ca5a0c2e0b804c3081a5cccd1d56bb7d458
Adriein <adria.claret@gmail.com>
2024-03-29 15:52:56 +0100

    making http request work

--- cmd/eb_guild_ops/main.go
+++ cmd/eb_guild_ops/main.go
@@ -1,1 +1,19 @@
-package eb_guild_ops
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/adriein/eb-guild-ops/internal/app_eb_guild_ops/repository"
+)
+
+func main() {
+	char, err := repository.Character("Marlock")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%#v\n", char)
+	a, err := json.Marshal(char)
+	fmt.Println(string(a))
+}
--- internal/app_eb_guild_ops/repository/tibia_data_api.go
+++ internal/app_eb_guild_ops/repository/tibia_data_api.go
@@ -1,35 +1,57 @@
 package repository
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 )
 
-type Date = time.Time
-
 type TibiaDataAPICharacter struct {
-	Name      string
-	LastLogin Date
-}
-
-type TibiaDataApiResponse[T any] struct {
-	Data    T
-	Success bool
+	Name          string
+	Traded        bool
+	DeletionDate  string
+	LastLogin     time.Time
+	AccountStatus string
 }
 
 type TibiaDataAPI interface {
-	Character() TibiaDataApiResponse[TibiaDataAPICharacter]
+	Character() (TibiaDataAPICharacter, error)
+}
+
+type TibiaDataAPICharacterJSON struct {
+	Character struct {
+		Character struct {
+			AccountStatus string `json:"account_status"`
+			DeletionDate  string `json:"deletion_date"`
+			LastLogin     string `json:"last_login"`
+			Traded        bool   `json:"traded"`
+		} `json:"character"`
+	} `json:"character"`
+	Information struct {
+		API struct {
+			Commit  string `json:"commit"`
+			Release string `json:"release"`
+			Version int    `json:"version"`
+		} `json:"api"`
+		Status struct {
+			Error    int    `json:"error"`
+			HTTPCode int    `json:"http_code"`
+			Message  string `json:"message"`
+		} `json:"status"`
+		Timestamp string `json:"timestamp"`
+	} `json:"information"`
 }
 
 const TIBIA_DATA_API_BASE_URL string = "https://api.tibiadata.com"
 const TIBIA_DATA_API_VERSION string = "v4"
 const TIBIA_DATA_API_CHARACTER_URL string = "character"
-const HTTP_GET_METHOD string = "GET"
 
-func Character(name string) TibiaDataApiResponse[TibiaDataAPICharacter] {
-	response, err := http.NewRequest(
-		HTTP_GET_METHOD,
+// Character make a new request to TibiaAPI and returns a TibiaDataAPICharacter struct
+func Character(name string) (TibiaDataAPICharacter, error) {
+	request, requestCreationError := http.NewRequest(
+		http.MethodGet,
 		fmt.Sprintf(
 			"%s/%s/%s/%s",
 			TIBIA_DATA_API_BASE_URL,
@@ -40,9 +62,44 @@ func Character(name string) TibiaDataApiResponse[TibiaDataAPICharacter] {
 		nil,
 	)
 
-	if err != nil {
-		return TibiaDataApiResponse[TibiaDataAPICharacter]{nil, false}
+	if requestCreationError != nil {
+		return TibiaDataAPICharacter{}, requestCreationError
 	}
 
-	return TibiaDataApiResponse[TibiaDataAPICharacter]{}
+	client := &http.Client{}
+	response, requestError := client.Do(request)
+
+	if requestError != nil {
+		return TibiaDataAPICharacter{}, requestError
+	}
+
+	defer response.Body.Close()
+
+	var apiResponse = TibiaDataAPICharacterJSON{}
+
+	byteRawResponse, readBodyError := io.ReadAll(response.Body)
+
+	if readBodyError != nil {
+		return TibiaDataAPICharacter{}, readBodyError
+	}
+
+	jsonUnmarshalError := json.Unmarshal(byteRawResponse, &apiResponse)
+
+	if jsonUnmarshalError != nil {
+		return TibiaDataAPICharacter{}, jsonUnmarshalError
+	}
+
+	lastLogin, timeParseError := time.Parse(time.RFC3339, apiResponse.Character.Character.LastLogin)
+
+	if timeParseError != nil {
+		return TibiaDataAPICharacter{}, timeParseError
+	}
+
+	return TibiaDataAPICharacter{
+		name,
+		apiResponse.Character.Character.Traded,
+		apiResponse.Character.Character.DeletionDate,
+		lastLogin,
+		apiResponse.Character.Character.AccountStatus,
+	}, nil
 }
